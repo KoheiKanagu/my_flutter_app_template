bd27906e1cb64267eb3e76fec3d6ca0385f3c388
KoheiKanagu <kanagu@kingu.dev>
2021-05-07 16:35:01 +0900

    rm

--- .github/workflows/scripts/buildiOS.sh
+++ .github/workflows/scripts/buildiOS.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -eu
-# 参考: https://qiita.com/KoheiKanagu/items/516f43686b3f96fc3022#cicd%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6
 
 if [ $# -ne 3 ]; then
   echo "e.g. ./buildiOS dev 1.2.3 100" 1>&2
