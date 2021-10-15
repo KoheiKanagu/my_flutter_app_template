b8fc108a4efc70bada3d354f9f7a91de609bad54
KoheiKanagu <kanagu@kingu.dev>
2021-10-15 17:18:59 +0900

    api key

--- .github/workflows/scripts/build_ios_dev.sh
+++ .github/workflows/scripts/build_ios_dev.sh
@@ -2,23 +2,31 @@
 
 flutter pub get
 
-APP_NAME=devアプリ # FIXME
-
+APP_NAME=devアプリ                                                                # FIXME
 APP_ASSOCIATED_DOMAIN=applinks:com.example.page.link                           # FIXME
 APP_PROVISIONING_PROFILE_SPECIFIER="dev.kingu.myFlutterAppTemplate.dev Ad Hoc" # FIXME
 BUNDLE_IDENTIFIER=dev.kingu.myFlutterAppTemplate.dev                           # FIXME
 
-fastlane run get_provisioning_profile \
-    username:'kanagu@kingu.dev' \
-    team_id:'4XBP3H82S7' \
-    app_identifier:$BUNDLE_IDENTIFIER \
-    adhoc:true \
-    readonly:true
+echo $FASTLANE_API_KEY_JSON >fastlaneApiKey.json
+
+# FASTLANE_API_KEY_JSON example
+# {
+#   "key_id": "12345",
+#   "issuer_id": "aaaa-bbbb-cccc-dddd-eeee",
+#   "key": "-----BEGIN PRIVATE KEY-----.....",
+#   "in_house": false
+# }
 
 plutil \
   -replace 'provisioningProfiles' \
   -json "{\"${BUNDLE_IDENTIFIER}\":\"${APP_PROVISIONING_PROFILE_SPECIFIER}\"}" \
   .github/workflows/scripts/AdHocExportOptions.plist
+
+fastlane run get_provisioning_profile \
+  api_key_path:fastlaneApiKey.json \
+  app_identifier:$BUNDLE_IDENTIFIER \
+  adhoc:true \
+  readonly:true
 
 flutter build ipa \
   --release \
@@ -26,7 +34,7 @@ flutter build ipa \
   --dart-define APP_SUFFIX=.dev \
   --dart-define APP_ENV=dev \
   --dart-define APP_ASSOCIATED_DOMAIN=$APP_ASSOCIATED_DOMAIN \
-  --dart-define APP_PROVISIONING_PROFILE_SPECIFIER="$APP_PROVISIONING_PROFILE_SPECIFIER"
+  --dart-define APP_PROVISIONING_PROFILE_SPECIFIER="$APP_PROVISIONING_PROFILE_SPECIFIER" \
   --export-options-plist=.github/workflows/scripts/AdHocExportOptions.plist
 
 echo "exported to build/ios/ipa/Runner.ipa"
--- .github/workflows/scripts/build_ios_prod.sh
+++ .github/workflows/scripts/build_ios_prod.sh
@@ -7,24 +7,33 @@ APP_ASSOCIATED_DOMAIN=applinks:com.example.page.link                       # FIX
 APP_PROVISIONING_PROFILE_SPECIFIER="dev.kingu.myFlutterAppTemplate Ad Hoc" # FIXME
 BUNDLE_IDENTIFIER=dev.kingu.myFlutterAppTemplate                           # FIXME
 
+echo $FASTLANE_API_KEY_JSON >fastlaneApiKey.json
+
+# FASTLANE_API_KEY_JSON example
+# {
+#   "key_id": "12345",
+#   "issuer_id": "aaaa-bbbb-cccc-dddd-eeee",
+#   "key": "-----BEGIN PRIVATE KEY-----.....",
+#   "in_house": false
+# }
+
 plutil \
   -replace 'provisioningProfiles' \
   -json "{\"${BUNDLE_IDENTIFIER}\":\"${APP_PROVISIONING_PROFILE_SPECIFIER}\"}" \
   .github/workflows/scripts/AdHocExportOptions.plist
 
 fastlane run get_provisioning_profile \
-    username:'kanagu@kingu.dev' \
-    team_id:'4XBP3H82S7' \
-    app_identifier:$BUNDLE_IDENTIFIER \
-    adhoc:true \
-    readonly:true
+  api_key_path:fastlaneApiKey.json \
+  app_identifier:$BUNDLE_IDENTIFIER \
+  adhoc:true \
+  readonly:true
 
 flutter build ipa \
   --release \
   --dart-define APP_NAME=$APP_NAME \
   --dart-define APP_ENV=prod \
   --dart-define APP_ASSOCIATED_DOMAIN=$APP_ASSOCIATED_DOMAIN \
-  --dart-define APP_PROVISIONING_PROFILE_SPECIFIER="$APP_PROVISIONING_PROFILE_SPECIFIER"
+  --dart-define APP_PROVISIONING_PROFILE_SPECIFIER="$APP_PROVISIONING_PROFILE_SPECIFIER" \
   --export-options-plist=.github/workflows/scripts/AdHocExportOptions.plist
 
 echo "exported to build/ios/ipa/Runner.ipa"
@@ -37,11 +46,10 @@ plutil \
   .github/workflows/scripts/AppStoreExportOptions.plist
 
 fastlane run get_provisioning_profile \
-    username:'kanagu@kingu.dev' \
-    team_id:'4XBP3H82S7' \
-    app_identifier:$BUNDLE_IDENTIFIER \
-    adhoc:false \
-    readonly:true
+  api_key_path:fastlaneApiKey.json \
+  app_identifier:$BUNDLE_IDENTIFIER \
+  adhoc:false \
+  readonly:true
 
 xcodebuild -exportArchive \
   -archivePath build/ios/archive/Runner.xcarchive \
@@ -51,5 +59,3 @@ echo "exported to build/ios/appStore/Runner.ipa"
 
 tar cfz build/ios/appStore.tar.gz build/ios/appStore
 echo "exported to build/ios/appStore.tar.gz"
-
-
